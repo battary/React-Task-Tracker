b6cb600a1e4939d295c883937733d8a4ad6806e6
Brian Camden <briancamden15@gmail.com>
2023-03-26 01:22:03 -0400

    Changed Markdown

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Button from './Button'
+
+const Header = ({title}) => {
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button />
+    </header>
+  )
+}
+
+export default Header
